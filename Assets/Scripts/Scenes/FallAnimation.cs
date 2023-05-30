e03ea21f900b61c5bfd7b04e47b6dbb8d8cca756
Daniel Benevides <danielfbenevides@gmail.com>
2023-05-30 14:59:20 -0300

    Loading screen falling animation

---
+++ Assets/Scripts/Scenes/FallAnimation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallAnimation : MonoBehaviour
+{
+    public float offsetY = 1.0f;
+    public float movementSpeed = 1.0f;
+
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        StartCoroutine(LoopMovement());
+    }
+
+    private IEnumerator LoopMovement()
+    {
+        while (true)
+        {
+            Vector3 targetPosition = originalPosition - new Vector3(0f, offsetY, 0f);
+
+            yield return MoveObject(targetPosition);
+            transform.position = originalPosition;
+        }
+    }
+
+    private IEnumerator MoveObject(Vector3 targetPosition)
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        float duration = distance / movementSpeed;
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+    }
+}
