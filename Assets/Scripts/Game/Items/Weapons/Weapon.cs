2b05548ec6f06635c410e52b85836f9e5361e462
Vinicius Gabriel <viniciusgabriel7711@gmail.com>
2023-05-31 01:15:57 -0300

    item gen + player animations improved

--- Assets/Scripts/Game/Items/Weapons/Weapon.cs
+++ Assets/Scripts/Game/Items/Weapons/Weapon.cs
@@ -1,17 +1,32 @@
+using UnityEngine;
 
 public class Weapon {
 
-    public static readonly float BASE_COOLDOWN = 0.3f;
+    public static readonly float BASE_COOLDOWN = 0.1f;
 
-    // TODO - Add meaningful names lol
-    public enum ANIMATION_TYPE
+    public static readonly int MAX_LEVEL = 10;
+
+    public readonly int Level;
+    public enum TYPE
     {
-        ANIM_1,
-        ANIM_2,
-        ANIM_3
+        AXE,
+        DAGGER,
+        HAMMER,
+        NUNCHAKU,
+        SICKLE,
+        SPEAR,
+        SWORD
     }
 
-    public ANIMATION_TYPE AnimType { get; set; }
+    public TYPE Type { get; set; }
+
+    // Order: axe, dagger, hammer, nunchaku, sickle, spear, sword
+
+    private static readonly int[] MAX_DMG = { 50, 25, 40, 15, 45, 30, 35 };
+
+    private static readonly int[] MAX_WEIGHT = { 10, 4, 8, 2, 9, 7, 6 };
+
+    private static readonly float[] MAX_RANGE = { 1.5f, 0.25f, 1f, 0.5f, 1.75f, 2f, 1.25f };
 
     private float baseDmg;
     public float BaseDmg
@@ -36,16 +51,25 @@ public class Weapon {
 
     public Weapon()
     {
+        Level = 0;
         BaseDmg = 0.0f;
         Range = 0.0f;
         Weight = 0.0f;
     }
 
-    public Weapon(float baseDmg_, float range_, float weight_)
+    public Weapon(int level, float baseDmg, float range, float weight)
     {
-        BaseDmg = baseDmg_;
-        Range = range_;
-        Weight = weight_;
-        // TODO - Range/Dmg logic to determine ANIM enum
+        Level = level;
+        BaseDmg = baseDmg;
+        Range = range;
+        Weight = weight;
+    }
+
+    public Weapon(int level, int type)
+    {
+        Level = level;
+        BaseDmg = Random.Range((float)(level - 1) / MAX_LEVEL * MAX_DMG[type] + 1, (float)level / MAX_LEVEL * MAX_DMG[type]);
+        Range = Random.Range((float)(level - 1) / MAX_LEVEL * MAX_RANGE[type] + 1, (float)level / MAX_LEVEL * MAX_RANGE[type]);
+        Weight = Random.Range((float)(level - 1) / MAX_LEVEL * MAX_WEIGHT[type] + 1, (float)level / MAX_LEVEL * MAX_WEIGHT[type]);
     }
 }
